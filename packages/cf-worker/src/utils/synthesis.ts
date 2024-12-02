790a374679fe8a45e66f3000e3faf3016564d434
DomKing <beixianchen@gmail.com>
2024-12-02 16:44:20 +0800

    Update synthesis.ts

--- packages/cf-worker/src/utils/synthesis.ts
+++ packages/cf-worker/src/utils/synthesis.ts
@@ -117,9 +117,6 @@ async function handleMessage(message: MessageEvent) {
         bufferData.subarray(2, contentIndex),
       );
       const requestId = parseRequestId(headers);
-      console.debug(
-        `Received binary/audio (${requestId})：length: ${bufferData.byteLength}`,
-      );
 
       return { requestId, data: bufferData.subarray(contentIndex) };
     }
